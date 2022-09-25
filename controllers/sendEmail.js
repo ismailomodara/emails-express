d18c2be40269e016adc142aa93a9c4fba82b7234
Ismail Omodara <ismailomodara@Ismails-MacBook-Pro-2.local>
2022-09-25 10:54:09 +0100

    Implemented SendGrid method

--- controllers/sendEmail.js
+++ controllers/sendEmail.js
@@ -1,14 +1,17 @@
 const nodemailer = require("nodemailer");
+const sgMail = require('@sendgrid/mail')
 
-const sendEmail = async (req, res) => {
-    // let testAccount = await nodemailer.createTestAccount();
+const welcome = require("../template/welcome");
+
+const sendEmailEthereal = async (req, res) => {
+    let testAccount = await nodemailer.createTestAccount();
 
     const transporter = nodemailer.createTransport({
         host: 'smtp.ethereal.email',
         port: 587,
         auth: {
-            user: 'gilda.lueilwitz@ethereal.email',
-            pass: 'ntaBYtG43Ehy2x2rKJ'
+            user: testAccount.user,
+            pass: testAccount.pass
         }
     });
 
@@ -17,13 +20,36 @@ const sendEmail = async (req, res) => {
         to: 'bar@example.com',
         subject: "Testing Email",
         text: "Hello world",
-        html: '<h2>Sending emails with Node</h2>'
+        html: welcome
     })
 
     res.status(200).json({
         success: true,
         info
     })
+}
+
+const sendEmail =  async (req, res) => {
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+    const msg = {
+        to: 'ismailomodara.io@gmail.com', // Change to your recipient
+        from: 'omodara145@gmail.com', // Change to your verified sender
+        subject: 'Sending with SendGrid is Fun',
+        text: 'and easy to do anywhere, even with Node.js',
+        html: welcome,
+    }
+    sgMail
+        .send(msg)
+        .then((response) => {
+            res.status(200).json({
+                success: true,
+                response
+            })
+            console.log('Email sent')
+        })
+        .catch((error) => {
+            console.error(error)
+        })
 }
 
 module.exports = { sendEmail }
---
+++ template/welcome.js
@@ -0,0 +1,274 @@
+const welcome = `<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta content="telephone=no" name="format-detection" />
+  <title></title>
+
+  <link href="https://fonts.googleapis.com/css2?family=PT+Serif:wght@700&display=swap" rel="stylesheet">
+
+  <!--[if mso]>
+  <style type="text/css">
+    body, table, td {font-family: Arial, Helvetica, sans-serif !important;}
+  </style>
+  <![endif]-->
+
+  <style>
+    @font-face {
+      font-family: 'Cerebri Sans';
+      src: url('https://res.cloudinary.com/eden-life-inc/raw/upload/v1588852944/email-template/fonts/CerebriSans-Regular_etbp7k.eot'); /* IE9 Compat Modes */
+      src: url('https://res.cloudinary.com/eden-life-inc/raw/upload/v1588852944/email-template/fonts/CerebriSans-Regular_etbp7k.eot?#iefix') format('embedded-opentype'), /* IE6-IE8 */
+      url('https://res.cloudinary.com/eden-life-inc/raw/upload/v1588852944/email-template/fonts/CerebriSans-Regular_kpdqct.woff') format('woff'), /* Modern Browsers */
+      url('https://res.cloudinary.com/eden-life-inc/raw/upload/v1588852944/email-template/fonts/CerebriSans-Regular_lsyolz.ttf') format('truetype'), /* Safari, Android, iOS */
+      url('https://raw.githubusercontent.com/matthieua/shecodes/1fbe8564dcaea4dea8b69d4cd83009781b1784cf/fonts/Cerebri-Sans/CerebriSans-Regular.svg#webfont') format('svg'); /* Legacy iOS */
+    }
+
+    body {
+      margin: 0;
+      padding: 0;
+      background-color: #fafcfc;
+      font-size: 15px;
+      line-height: 1.6;
+      color: #444444;
+      width: 100%;
+      font-family: 'Cerebri Sans', sans-serif, Open Sans, -apple-system, BlinkMacSystemFont, Roboto, Helvetica Neue, Helvetica, Arial, sans-serif;
+    }
+
+    img {
+      border: 0 none;
+      line-height: 100%;
+      outline: none;
+      text-decoration: none;
+      vertical-align: baseline;
+      font-size: 0;
+    }
+
+    a:hover {
+      text-decoration: underline;
+    }
+
+    .btn {
+      padding: 12px 25px;
+      vertical-align: middle;
+      color: #fff;
+      font-family: 'Cerebri Sans', sans-serif;
+      font-size: 14px;
+      border-radius: 4px;
+      display: inline-block;
+    }
+
+    .btn-primary {
+      background-color: #03A84E;
+    }
+
+    h1, h2, h3, h4, h5, h6 {
+      font-family: 'PT Serif', serif;
+    }
+
+    p {
+      font-family: 'Cerebri Sans', sans-serif, Open Sans, -apple-system, BlinkMacSystemFont, Roboto, Helvetica Neue, Helvetica, Arial, sans-serif;
+    }
+
+    @media only screen and (max-width: 560px) {
+      body {
+        font-size: 14px !important;
+      }
+
+      .bg-image {
+        height: 200px !important;
+      }
+
+      .content {
+        padding: 10px 24px !important;
+      }
+
+      .footer {
+        padding-left: 24px !important;
+        padding-right: 24px !important;
+      }
+
+      .footer > div {
+        padding-right: 30px !important;
+        padding-left: 30px !important;
+      }
+
+      .footer > div td {
+        padding-left: 0 !important;
+        padding-right: 0 !important;
+      }
+
+      .footer .app-link {
+        width: 110px;
+      }
+
+      h1 {
+        font-size: 24px !important;
+      }
+
+      h2 {
+        font-size: 20px !important;
+      }
+
+      h3 {
+        font-size: 18px !important;
+      }
+    }
+  </style>
+</head>
+
+<body class="bg-body"
+      style="font-size: 15px; line-height: 1.6; color: #444444; width: 100%; height: 100vh; overflow-y: scroll; font-family: Open Sans, -apple-system, BlinkMacSystemFont, Roboto, Helvetica Neue, Helvetica, Arial, sans-serif; margin: 0; padding: 0;"
+      bgcolor="#F2FFF6">
+<table class="main bg-body" width="100%" cellspacing="0" cellpadding="0"
+       style="height: 100%; font-family: Open Sans, -apple-system, BlinkMacSystemFont, Roboto, Helvetica Neue, Helvetica, Arial, sans-serif; border-collapse: collapse; width: 100%; -webkit-text-size-adjust: 100%; -ms-text-size-adjust: 100%;box-shadow: 0 6px 16px 0 rgba(234, 238, 239, 0.2); border-radius: 4px;"
+       bgcolor="#F2FFF6">
+  <tr>
+    <td align="center" valign="top"
+        style="font-family: Open Sans, -apple-system, BlinkMacSystemFont, Roboto, Helvetica Neue, Helvetica, Arial, sans-serif;">
+      <!--[if (gte mso 9)|(IE)]>
+      <table border="0" cellspacing="0" cellpadding="0">
+        <tr>
+          <td align="center" valign="top" width="640">
+      <![endif]-->
+      <table class="wrap" cellspacing="0" cellpadding="0"
+             style="font-family: Open Sans, -apple-system, BlinkMacSystemFont, Roboto, Helvetica Neue, Helvetica, Arial, sans-serif; border-radius: 4px; border-collapse: collapse; width: 100%; max-width: 720px; text-align: left; margin-top: 80px;">
+        <tr>
+          <td class="p-sm"
+              style="font-family: Open Sans, -apple-system, BlinkMacSystemFont, Roboto, Helvetica Neue, Helvetica, Arial, sans-serif; padding: 8px;">
+            <div class="main-content">
+              <table class="box" cellpadding="0" cellspacing="0"
+                     style="font-family: Open Sans, -apple-system, BlinkMacSystemFont, Roboto, Helvetica Neue, Helvetica, Arial, sans-serif; min-height: 600px; border-collapse: collapse; width: 100%; border-radius: 2px;">
+                <tr>
+                  <td
+                          style="font-family: Open Sans, -apple-system, BlinkMacSystemFont, Roboto, Helvetica Neue, Helvetica, Arial, sans-serif;">
+                    <table cellpadding="0" cellspacing="0"
+                           style="font-family: Open Sans, -apple-system, BlinkMacSystemFont, Roboto, Helvetica Neue, Helvetica, Arial, sans-serif; border-collapse: collapse; width: 100%;">
+                      <tr>
+                        <td class="content text-wrap"
+                            style="background: #fff;font-family:Open Sans, -apple-system, BlinkMacSystemFont, Roboto, Helvetica Neue, Helvetica, Arial, sans-serif; padding: 60px 80px 40px;">
+                          <a href="https://ouredenlife.com"><img src="https://res.cloudinary.com/eden-life-inc/image/upload/v1588856254/email-template/eden-logo_ihisls.png" alt="Eden" /></a>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td class="content text-wrap"
+                            style="background: #fff;font-family:Open Sans, -apple-system, BlinkMacSystemFont, Roboto, Helvetica Neue, Helvetica, Arial, sans-serif; padding: 0 80px 20px;">
+                          <h3 style="font-weight: 600; font-family: 'PT Serif', serif; font-size: 20px; line-height: 1.6; margin: 0 0 15px; color: #21312A
+;">Hello {{ customerName }}</h3>
+                          <p>A celebratory bell just went off at the Eden HQ, and it's because we're glad to have you here!</p>
+                          <p style="margin-bottom: 0; line-height: 2; font-size: 14px;">Your Eden Life is about to begin, and this is how our relationship begins:</p>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td class="content text-wrap"
+                          style="background: #fff;padding: 0 80px 20px; border-bottom-left-radius: 12px; border-bottom-right-radius: 12px;">
+                          <table width="100%" cellspacing="0" cellpadding="0"
+                            style="border-collapse: collapse;">
+                            <tr>
+                              <th style="background: #E4E8E650; border-top-left-radius: 8px; padding: 12px 20px; font-family: 'Cerebri Sans', sans-serif, Open Sans, -apple-system, BlinkMacSystemFont, Roboto, Helvetica Neue, Helvetica, Arial, sans-serif;font-size: 14px;">Service</th>
+                              <th style="background: #E4E8E650; border-top-right-radius: 8px; padding: 12px 20px; font-family: 'Cerebri Sans', sans-serif, Open Sans, -apple-system, BlinkMacSystemFont, Roboto, Helvetica Neue, Helvetica, Arial, sans-serif;font-size: 14px;">Frequency</th>
+                            </tr>
+                            <tr>
+                              <td style="border: 1px solid rgba(228, 232, 230, 0.4); padding: 12px 20px; font-family: 'Cerebri Sans', sans-serif, Open Sans, -apple-system, BlinkMacSystemFont, Roboto, Helvetica Neue, Helvetica, Arial, sans-serif;font-size: 14px;">Cleaning</th>
+                              <td style="border: 1px solid rgba(228, 232, 230, 0.4); padding: 12px 20px; font-family: 'Cerebri Sans', sans-serif, Open Sans, -apple-system, BlinkMacSystemFont, Roboto, Helvetica Neue, Helvetica, Arial, sans-serif;font-size: 14px;">Twice a week</th>
+                            </tr>
+                            <tr>
+                              <td style="border: 1px solid rgba(228, 232, 230, 0.4); padding: 12px 20px; font-family: 'Cerebri Sans', sans-serif, Open Sans, -apple-system, BlinkMacSystemFont, Roboto, Helvetica Neue, Helvetica, Arial, sans-serif;font-size: 14px;">Laundry</th>
+                              <td style="border: 1px solid rgba(228, 232, 230, 0.4); padding: 12px 20px; font-family: 'Cerebri Sans', sans-serif, Open Sans, -apple-system, BlinkMacSystemFont, Roboto, Helvetica Neue, Helvetica, Arial, sans-serif;font-size: 14px;">Monthly</th>
+                            </tr>
+                            <tr>
+                              <td style="border: 1px solid rgba(228, 232, 230, 0.4); padding: 20px; font-family: 'Cerebri Sans', sans-serif, Open Sans, -apple-system, BlinkMacSystemFont, Roboto, Helvetica Neue, Helvetica, Arial, sans-serif;font-size: 14px;">Total</th>
+                              <td style="border: 1px solid rgba(228, 232, 230, 0.4); padding: 20px; font-family: 'Cerebri Sans', sans-serif, Open Sans, -apple-system, BlinkMacSystemFont, Roboto, Helvetica Neue, Helvetica, Arial, sans-serif;font-size: 14px; font-size: 18px;
+                              font-weight: 600;
+                              color: #21312A;
+                              margin: 0;">N40,000.00</th>
+                            </tr>
+                          </table>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td class="content text-wrap"
+                            style="background: #fff;font-family:Open Sans, -apple-system, BlinkMacSystemFont, Roboto, Helvetica Neue, Helvetica, Arial, sans-serif; padding: 0 80px 20px;">
+                          <p style="margin-bottom: 0; line-height: 2; font-size: 14px;">Next, download the app. The services you've selected will already be there, and you can proceed to pay, and start your Eden Life.</p>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td class="content text-wrap"
+                            style="background: #fff;font-family:Open Sans, -apple-system, BlinkMacSystemFont, Roboto, Helvetica Neue, Helvetica, Arial, sans-serif; padding: 0 20px 20px;">
+
+                          <div style="padding:10px 0 30px; text-align: center;">
+                            <a href="https://play.google.com/store/apps/details?id=com.ouredenlife.app"><img style="margin-right: 10px;" src="https://res.cloudinary.com/eden-life-inc/image/upload/v1588960954/email-template/Google_Play_Store_badge_EN_rus0ez.png" alt /></a>
+                            <a href="https://apps.apple.com/us/app/eden-life/id1482373755?ls=1"><img src="https://res.cloudinary.com/eden-life-inc/image/upload/v1588960954/email-template/Download_on_the_App_Store_Badge_yny2oe.png" alt /></a>
+                          </div>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td class="footer" align="center" style="padding: 0 80px; background: #fff">
+                          <div style="height: 100%; background: #03A84E; border-top-left-radius: 8px; border-top-right-radius: 8px; padding: 20px 40px 30px;">
+                            <table class="w-auto" cellspacing="0" cellpadding="0"
+                                   style="width: 100%; border-top-left-radius: 8px; border-top-right-radius: 8px; margin: auto;font-family: Open Sans, -apple-system, BlinkMacSystemFont, Roboto, Helvetica Neue, Helvetica, Arial, sans-serif; border-collapse: collapse; background: #03A84E;">
+                              <tr>
+                                <td>
+                                  <img style="height: 28px; margin-bottom: 12px; width: auto;" src="https://res.cloudinary.com/eden-life-inc/image/upload/v1588863218/email-template/eden_logo_white_2x_wvhgrz.png" alt=""/>
+                                </td>
+                                <td style="width: 100%; text-align: right; vertical-align: middle">
+                                  <a href="https://blog.ouredenlife.com" style="color: #fff; text-decoration: underline; font-size: 12px;">Check out our blog</a>
+                                </td>
+                              </tr>
+                            </table>
+                          </div>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td class="footer"  align="center" style="padding: 0 80px 40px; background: #fff">
+                          <div style="height: 100%; background: #03A84E;border-bottom-left-radius: 8px; border-bottom-right-radius: 8px;">
+                            <table class="w-auto" cellspacing="0" cellpadding="0"
+                                   style="width: 100%; border-bottom-left-radius: 8px; border-bottom-right-radius: 8px; margin: auto;font-family: Open Sans, -apple-system, BlinkMacSystemFont, Roboto, Helvetica Neue, Helvetica, Arial, sans-serif; border-collapse: collapse; background: #03A84E;">
+                              <tr>
+                                <td style="padding: 0 40px 30px; color: #fff; text-align: center">
+                                  <a href="https://facebook.com/ouredenlife" style="padding: 0 10px;"><img style="height: 24px;" src="https://res.cloudinary.com/eden-life-inc/image/upload/v1588863798/email-template/facebook-box-fill_iqlfgt.png" alt="Facebook" /></a>
+                                  <a href="https://instagram.com/ouredenlife" style="padding: 0 10px;"><img style="height: 24px;" src="https://res.cloudinary.com/eden-life-inc/image/upload/v1588863797/email-template/instagram-fill_ith2fr.png" alt="Instagram" /></a>
+                                  <a href="https://www.linkedin.com/company/ouredenlife/" style="padding: 0 10px;"><img style="height: 24px;" src="https://res.cloudinary.com/eden-life-inc/image/upload/v1588863798/email-template/linkedin-box-fill_ksfvlx.png" alt="LinkedIn" /></a>
+                                  <a href="https://twitter.com/ouredenlife" style="padding-left: 10px"><img style="height: 24px;" src="https://res.cloudinary.com/eden-life-inc/image/upload/v1588863798/email-template/twitter-fill_kqsdin.png" alt="Twitter" /></a>
+                                </td>
+                              </tr>
+                              <tr>
+                                <td style="padding: 0 40px 30px; color: #fff; text-align: center">
+                                  <p style="font-family: 'Cerebri Sans', sans-serif; margin: 0 0 5px; font-size: 14px;">Our postal address:</p>
+                                  <p style="font-weight: 600; font-family: 'Cerebri Sans', sans-serif; margin: 0; font-size: 14px; color: #fff;">C7 U3, Jacob Mews Estate, Adebisi Street, Yaba.</a>
+                                </td>
+                              </tr>
+                              <tr>
+                                <td style="padding: 0 40px 20px; color: #fff; text-align: center">
+                                  <a style="color: #fff; font-size: 12px; text-decoration: none; padding-right: 12px" href="https://ouredenlife.com/food">Food</a>
+                                  <a style="color: #fff; font-size: 12px; text-decoration: none; padding: 0 12px" href="https://ouredenlife.com/laundry">Laundry</a>
+                                  <a style="color: #fff; font-size: 12px; text-decoration: none; padding: 0 12px" href="https://ouredenlife.com/cleaning">Home Cleaning</a>
+                                  <a style="color: #fff; font-size: 12px; text-decoration: none; padding: 0 12px" href="https://ouredenlife.com/companies">Companies</a>
+                                </td>
+                              </tr>
+                            </table>
+                          </div>
+                        </td>
+                      </tr>
+                    </table>
+                  </td>
+                </tr>
+              </table>
+            </div>
+          </td>
+        </tr>
+      </table>
+      </div>
+    </td>
+  </tr>
+</table>
+<!--[if (gte mso 9)|(IE)]>
+</td>
+</tr>
+</table>
+<![endif]-->
+</td>
+</tr>
+</table>
+</body>
+`
+
+module.exports = welcome
